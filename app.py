23508a7dfaab21c9b5d318b8d35fc9602da2785c
Kasra Mojallal <kasramojallal1@gmail.com>
2024-03-18 12:28:54 -0400

    app was improved for inputs

--- app.py
+++ app.py
@@ -12,24 +12,33 @@ def main():
     people = []
     submit_flag = False
 
+    skills = []
+    experience = []
+    salary = []
+
     dataset = st.selectbox("Choose Dataset", ["free-lancer", "jd-job"])
 
     st.title("Team Information")
 
     col1, col2, col3 = st.columns(3)
-    skills = col1.text_input("Skills", "")
-    experience = col2.selectbox("Experience", ["Entry Level", "Mid Level", "Senior Level"])
-    salary = col3.text_input("Salary", "")
+    skills1 = col1.text_input("Skills", "")
+    experience1 = col2.selectbox("Experience", ["Entry Level", "Mid Level", "Senior Level"])
+    salary1_min, salary1_max = col3.columns(2)
+    salary1_min = salary1_min.number_input("Salary1 Min", min_value=0, value=10, step=1)
+    salary1_max = salary1_max.number_input("Salary1 Max", min_value=0, value=100, step=1)
+
     col1, col2, col3 = st.columns(3)
     skills2 = col1.text_input("Skills2", "")
     experience2 = col2.selectbox("Experience2", ["Entry Level", "Mid Level", "Senior Level"])
-    salary2 = col3.text_input("Salary2", "")
+    salary2_min, salary2_max = col3.columns(2)
+    salary2_min = salary2_min.number_input("Salary2 Min", min_value=0, value=10, step=1)
+    salary2_max = salary2_max.number_input("Salary2 Max", min_value=0, value=100, step=1)
 
     if st.button("Submit"):
         submit_flag = True
-        people.append([skills, experience, salary])
+        people.append([skills1, experience1, salary1_min, salary1_max])
         if skills2:
-            people.append([skills2, experience2, salary2])
+            people.append([skills2, experience2, salary2_min, salary2_max])
 
         if dataset == "free-lancer":
             df = pd.read_csv('./datasets/freelance-projects.csv')
@@ -38,9 +47,23 @@ def main():
 
 
     if len(people) == 1:
-        pass
+        skills = [skill.strip() for skill in skills1.split(',')]
+        experience = experience1
+        salary = [salary1_min, salary1_max]
+
+        st.write('Skills:', skills)
+        st.write('Experience:', experience)
+        st.write('Salary:', salary)
+        
     elif len(people) > 1:
-        pass
+        skills = [skill.strip() for skill in skills1.split(',')]
+        skills.extend([skill.strip() for skill in skills2.split(',')])
+        experience = max(experience1, experience2)
+        salary = [salary1_min + salary2_min, salary1_max + salary2_max]
+
+        st.write('Skills:', skills)
+        st.write('Experience:', experience)
+        st.write('Salary:', salary)
 
     if (submit_flag == True and dataset == "free-lancer"):
         df = df.drop('Date Posted', axis=1)
